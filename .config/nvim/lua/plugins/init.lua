1ded11e2ed8e2cae20e540200385bf8f1a76e0b2
Martin Eldredge <martin.eldredge1@gmail.com>
2026-08-17 10:50:00 -0600

    added latex stuff

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -61,6 +61,29 @@ local default_plugins = {
   },
 
   {
+      "lervag/vimtex",
+      lazy = false,
+      init = function()
+          vim.g.vimtex_view_method = "zathura"
+          vim.g.vimtex_compiler_method = "latexmk"
+          vim.g.vimtex_compiler_latexmk = {
+              aux_dir = "",
+              out_dir = "",
+              callback = 1,
+              continuous = 1,
+              executable = "latexmk",
+              hooks = {},
+              options = {
+                  "-verbose",
+                  "-file-line-error",
+                  "-synctex=1",
+                  "-interaction=nonstopmode",
+              },
+          }
+      end,
+  },
+
+  {
     "lukas-reineke/indent-blankline.nvim",
     dependencies = { "nvim-treesitter/nvim-treesitter" },
   },
